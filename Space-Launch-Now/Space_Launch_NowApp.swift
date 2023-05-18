2a2ccc728074540407c61bdbf38ddf68a46fa948
Faizan Tanveer <faizan.tanveer@brainxtech.com>
2023-05-19 01:51:30 +0500

    Initial Commit

---
+++ Space-Launch-Now/Space_Launch_NowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Space_Launch_NowApp.swift
+//  Space-Launch-Now
+//
+//  Created by EAPPLE on 19/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Space_Launch_NowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
